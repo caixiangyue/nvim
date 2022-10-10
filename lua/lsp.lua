c08e3e87c2bf017fe79917c517cea1f7746d8879
caixiangyue <caixiangyue007@gmail.com>
2022-10-10 17:43:32 +0800

    update

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -119,7 +119,7 @@ end
   local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
  -- Use a loop to conveniently both setup defined servers 
  -- and map buffer local keybindings when the language server attaches
- local servers = { "erlangls" , "gopls", "ccls", "jedi_language_server", "rust_analyzer"}
+ local servers = { "erlangls" , "gopls", "clangd", "jedi_language_server", "rust_analyzer"}
  for _, lsp in ipairs(servers) do
   nvim_lsp[lsp].setup { on_attach = on_attach , capabilities = capabilities}
  end
