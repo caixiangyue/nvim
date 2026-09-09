3622b2716c03d20d00b8c946029bb34a212b61b5
caixiangyue <caixiangyue007@gmail.com>
2026-09-09 20:13:17 +0800

    update

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,7 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+vim.opt.tabstop = 4       -- 一个 Tab 显示的宽度为 4 个空格
+vim.opt.shiftwidth = 4    -- 自动缩进（>>、<<、=）的宽度为 4
+vim.opt.softtabstop = 4   -- 编辑时按下 Tab 键插入的空格数
+vim.opt.expandtab = true  -- 将 Tab 键转换为空格（推荐）
